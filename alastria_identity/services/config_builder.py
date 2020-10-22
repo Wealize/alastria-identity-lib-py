900ae579ff4bdeb1e7a7cf7247ec8376438267f6
Javier Aguirre <contacto@javaguirre.net>
2020-10-22 11:22:24 +0200

    Config builder refactor

--- alastria_identity/services/config_builder.py
+++ alastria_identity/services/config_builder.py
@@ -7,17 +7,25 @@ from .parsers import ContractParser
 
 
 class IdentityConfigBuilder:
+    CONTRACT_NAME_REGEX = r'sol_(?P<name>.*)\.abi'
+    CONTRACT_URL_POSITION = -2
+
     def __init__(self, contracts_info_url: str, parser_class: ContractParser):
         self.parser_class = parser_class
         self.contracts_info_url = contracts_info_url
 
     def get_contracts(self) -> List[str]:
         contracts_raw_response = requests.get(self.contracts_info_url)
-        contracts = map(
-            lambda contract_line: contract_line.split('|')[-2].strip(),
-            contracts_raw_response.content.decode('utf-8').split('\n')[2:-1]
+        return list(self.extract_urls_from_response(
+            contracts_raw_response.content.decode('utf-8')))
+
+    def extract_urls_from_response(
+        self, contracts_content: List[dict]
+    ) -> List[str]:
+        return map(
+            lambda contract_line: contract_line.split('|')[self.CONTRACT_URL_POSITION].strip(),
+            contracts_content.split('\n')[2:-1]
         )
-        return list(contracts)
 
     def generate(self):
         config = {}
@@ -25,7 +33,7 @@ class IdentityConfigBuilder:
 
         for contract_url in contracts:
             name = re.search(
-                r'sol_(?P<name>.*)\.abi',
+                self.CONTRACT_NAME_REGEX,
                 contract_url
             ).group('name')
             config[name] = self.parser_class(contract_url).parse()
