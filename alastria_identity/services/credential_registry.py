b4c462877b914f10f7b4808167661c1c203002c7
Daniel Luque Quintana <dranko91@gmail.com>
2020-12-16 16:08:01 +0100

    Indent code

--- alastria_identity/services/credential_registry.py
+++ alastria_identity/services/credential_registry.py
@@ -11,34 +11,55 @@ class CredentialRegistryService:
         self.endpoint = endpoint
 
     def add_subject_credential(self, subject_credential_hash: str, uri: str) -> Transaction:
-        return self._build_transaction("addSubjectCredential", [subject_credential_hash, uri], delegated=True)
+        return self._build_transaction(
+            "addSubjectCredential",
+            [subject_credential_hash, uri],
+            delegated=True)
 
     def delete_subject_credential(self, subject_credential_hash: str) -> Transaction:
-        return self._build_transaction("deleteSubjectCredential", [subject_credential_hash], delegated=True)
+        return self._build_transaction(
+            "deleteSubjectCredential",
+            [subject_credential_hash],
+            delegated=True)
 
     def get_subject_credential_status(self, subject_did: str, subject_credential_hash: str) -> Transaction:
         subject_address = NetworkDid.from_did(subject_did).proxy_address
-        return self._build_transaction("getSubjectCredentialStatus", [subject_address, subject_credential_hash], delegated=False)
+        return self._build_transaction(
+            "getSubjectCredentialStatus",
+            [subject_address, subject_credential_hash],
+            delegated=False)
 
     def get_subject_credential_list(self, subject_did: str) -> Transaction:
         subject_address = NetworkDid.from_did(subject_did).proxy_address
-        return self._build_transaction("getSubjectCredentialList", [subject_address], delegated=False)
+        return self._build_transaction(
+            "getSubjectCredentialList",
+            [subject_address],
+            delegated=False)
 
     def add_issuer_credential(self, issuer_credential_hash: str) -> Transaction:
-        return self._build_transaction("addIssuerCredential", [issuer_credential_hash], delegated=True)
+        return self._build_transaction(
+            "addIssuerCredential",
+            [issuer_credential_hash],
+            delegated=True)
 
     def get_issuer_credential_status(self, issuer_did: str, issuer_credential_hash: str) -> Transaction:
         issuer_address = NetworkDid.from_did(issuer_did).proxy_address
-        return self._build_transaction("getIssuerCredentialStatus", [
-                                issuer_address, issuer_credential_hash], delegated=False)
+        return self._build_transaction(
+            "getIssuerCredentialStatus",
+            [issuer_address, issuer_credential_hash],
+            delegated=False)
 
     def update_credential_status(self, issuer_credential_hash: str, status: int) -> Transaction:
-        return self._build_transaction("updateCredentialStatus", [
-                                issuer_credential_hash, status], delegated=True)
+        return self._build_transaction(
+            "updateCredentialStatus",
+            [issuer_credential_hash, status],
+            delegated=True)
 
     def get_credential_status(self, subject_status: int, issuer_status: int) -> Transaction:
-        return self._build_transaction("getCredentialStatus", [
-                                subject_status, issuer_status], delegated=False)
+        return self._build_transaction(
+            "getCredentialStatus",
+            [subject_status, issuer_status],
+            delegated=False)
 
     def _build_transaction(self, function_name: str, args: list, delegated: bool) -> Transaction:
         encoded_abi = ContractsService.AlastriaCredentialRegistry(self.endpoint).encodeABI(
