cb8f78de7cdd6ff9c0213a8f317cac9fc8331ae4
Daniel Luque Quintana <dranko91@gmail.com>
2020-12-28 12:44:21 +0100

    Fix outdated type name

--- alastria_identity/services/identity.py
+++ alastria_identity/services/identity.py
@@ -2,24 +2,24 @@ from typing import List
 
 from eth_account.account import SignedTransaction, HexBytes
 
-from alastria_identity.types import UserIdentity, BasicTransaction
+from alastria_identity.types import UserIdentity, Transaction
 
 
 class UserIdentityService:
     def __init__(self, identity: UserIdentity):
         self.identity = identity
 
-    def add_transaction(self, transaction: BasicTransaction) -> None:
+    def add_transaction(self, transaction: Transaction) -> None:
         self.identity.transactions.append(transaction)
 
     def get_signed_transactions(self) -> List:
         return map(self.sign_transaction, self.identity.transactions)
 
-    def get_signed_transaction_from_anonymous(self, transaction: BasicTransaction) -> HexBytes:
+    def get_signed_transaction_from_anonymous(self, transaction: Transaction) -> HexBytes:
         user_transaction = self.update_transaction_with_user_data(transaction)
         return self.sign_transaction(user_transaction)
 
-    def update_transaction_with_user_data(self, transaction: BasicTransaction) -> BasicTransaction:
+    def update_transaction_with_user_data(self, transaction: Transaction) -> Transaction:
         transaction.nonce = self.get_user_nonce()
         transaction.gasPrice = 0
         return transaction
