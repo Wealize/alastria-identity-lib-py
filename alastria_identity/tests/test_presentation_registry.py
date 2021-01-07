e60e4a485d69027bdf8727aac024f26578fcf90a
Daniel Ranchal <daniranchal@wealize.digital>
2021-01-07 17:47:24 +0100

    Finishing tests

--- alastria_identity/tests/test_presentation_registry.py
+++ alastria_identity/tests/test_presentation_registry.py
@@ -1,4 +1,4 @@
-from mock import *
+from mock import Mock, patch
 from dataclasses import asdict
 from unittest.mock import patch
 from web3 import Web3
@@ -9,8 +9,8 @@ from alastria_identity.services import PresentationRegistryService, IDENTITY_MAN
 @patch('alastria_identity.services.PresentationRegistryService.delegated')
 @patch('alastria_identity.services.identity_manager.ContractsService.AlastriaPresentationRegistry')
 def test_add_subject_presentation(
-        mock_alastria_presentation_registry,
-        mock_delegated
+    mock_alastria_presentation_registry,
+    mock_delegated
 ):
 
     web3_mock = Mock()
@@ -49,8 +49,8 @@ def test_add_subject_presentation(
 @patch('alastria_identity.services.PresentationRegistryService.delegated')
 @patch('alastria_identity.services.identity_manager.ContractsService.AlastriaPresentationRegistry')
 def test_update_subject_presentation(
-        mock_alastria_presentation_registry,
-        mock_delegated
+    mock_alastria_presentation_registry,
+    mock_delegated
 ):
 
     web3_mock = Mock()
@@ -88,68 +88,96 @@ def test_update_subject_presentation(
     assert asdict(transaction) == expected_transaction
 
 
-@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaCredentialRegistry')
-def test_get_subject_credential_status(
-        mock_alastria_credential_registry):
+@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaPresentationRegistry')
+def test_get_subject_presentation_status(
+    mock_alastria_presentation_registry
+):
+
     web3_mock = Mock()
-    subject_credential_hash = '0x1234'
+    subject_presentation_hash = '0x1234'
     subject_address = 'e53d78c1c6fc694a0f29b3f24bee439338acbe3e'
     subject_did = f'did:ala:quor:redT:{subject_address}'
-    mock_alastria_credential_registry(
-        web3_mock).encodeABI.return_value = 'getSubjectCredentialStatusReturnValue'
+
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.return_value = 'getSubjectPresentationStatusReturnValue'
+
     expected_transaction = {
         'to': Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS),
-        'data': 'getSubjectCredentialStatusReturnValue',
+        'data': 'getSubjectPresentationStatusReturnValue',
         'gasPrice': 0,
         'gas': 600000,
         'nonce': '0x0'
     }
-    service = CredentialRegistryService(web3_mock)
 
-    transaction = service.get_subject_credential_status(
-        subject_did, subject_credential_hash)
+    service = PresentationRegistryService(web3_mock)
 
-    mock_alastria_credential_registry.assert_called_with(web3_mock)
-    mock_alastria_credential_registry(web3_mock).encodeABI.assert_called_with(
-        fn_name='getSubjectCredentialStatus', args=[subject_address, subject_credential_hash])
+    transaction = service.get_subject_presentation_status(
+        subject_did, subject_presentation_hash
+    )
+
+    mock_alastria_presentation_registry.assert_called_with(web3_mock)
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.assert_called_with(
+        fn_name='getSubjectPresentationStatus',
+        args=[subject_address, subject_presentation_hash]
+    )
+
     assert asdict(transaction) == expected_transaction
 
 
-@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaCredentialRegistry')
-def test_get_subject_credential_list(
-        mock_alastria_credential_registry):
+@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaPresentationRegistry')
+def test_get_subject_presentation_list(
+    mock_alastria_presentation_registry
+):
+
     web3_mock = Mock()
     subject_address = 'e53d78c1c6fc694a0f29b3f24bee439338acbe3e'
     subject_did = f'did:ala:quor:redT:{subject_address}'
-    mock_alastria_credential_registry(
-        web3_mock).encodeABI.return_value = 'getSubjectCredentialListReturnValue'
+
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.return_value = 'getSubjectPresentationListReturnValue'
+
     expected_transaction = {
         'to': Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS),
-        'data': 'getSubjectCredentialListReturnValue',
+        'data': 'getSubjectPresentationListReturnValue',
         'gasPrice': 0,
         'gas': 600000,
         'nonce': '0x0'
     }
-    service = CredentialRegistryService(web3_mock)
 
-    transaction = service.get_subject_credential_list(subject_did)
+    service = PresentationRegistryService(web3_mock)
 
-    mock_alastria_credential_registry.assert_called_with(web3_mock)
-    mock_alastria_credential_registry(web3_mock).encodeABI.assert_called_with(
-        fn_name='getSubjectCredentialList', args=[subject_address])
+    transaction = service.get_subject_presentation_list(subject_did)
+
+    mock_alastria_presentation_registry.assert_called_with(web3_mock)
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.assert_called_with(
+        fn_name='getSubjectPresentationList',
+        args=[subject_address]
+    )
+
     assert asdict(transaction) == expected_transaction
 
 
-@patch('alastria_identity.services.CredentialRegistryService.delegated')
-@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaCredentialRegistry')
-def test_add_issuer_credential(
-        mock_alastria_credential_registry,
-        mock_delegated):
+@patch('alastria_identity.services.PresentationRegistryService.delegated')
+@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaPresentationRegistry')
+def test_update_receiver_presentation(
+    mock_alastria_presentation_registry,
+    mock_delegated
+):
+
     web3_mock = Mock()
-    issuer_credential_hash = '0x1234'
-    mock_alastria_credential_registry(
-        web3_mock).encodeABI.return_value = 'addIssuerCredentialReturnValue'
+    receiver_presentation_hash = '0x1234'
+    status = 1
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.return_value = 'updateReceiverPresentationReturnValue'
     mock_delegated.return_value = 'delegatedReturnValue'
+
     expected_transaction = {
         'to': Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS),
         'data': 'delegatedReturnValue',
@@ -157,112 +185,116 @@ def test_add_issuer_credential(
         'gas': 600000,
         'nonce': '0x0'
     }
-    service = CredentialRegistryService(web3_mock)
 
-    transaction = service.add_issuer_credential(issuer_credential_hash)
+    service = PresentationRegistryService(web3_mock)
 
-    mock_alastria_credential_registry.assert_called_with(web3_mock)
-    mock_alastria_credential_registry(web3_mock).encodeABI.assert_called_with(
-        fn_name='addIssuerCredential', args=[issuer_credential_hash])
-    mock_delegated.assert_called_with('addIssuerCredentialReturnValue')
+    transaction = service.update_receiver_presentation(receiver_presentation_hash, status)
+
+    mock_alastria_presentation_registry.assert_called_with(web3_mock)
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.assert_called_with(
+        fn_name='updateReceiverPresentation',
+        args=[receiver_presentation_hash, status]
+    )
+    mock_delegated.assert_called_with('updateReceiverPresentationReturnValue')
+
     assert asdict(transaction) == expected_transaction
 
 
-@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaCredentialRegistry')
-def test_get_issuer_credential_status(
-        mock_alastria_credential_registry):
+@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaPresentationRegistry')
+def test_get_receiver_presentation_status(
+    mock_alastria_presentation_registry
+):
+
     web3_mock = Mock()
-    issuer_address = 'e53d78c1c6fc694a0f29b3f24bee439338acbe3e'
-    issuer_did = f'did:ala:quor:redT:{issuer_address}'
-    issuer_credential_hash = '0x1234'
-    mock_alastria_credential_registry(
-        web3_mock).encodeABI.return_value = 'getIssuerCredentialStatusReturnValue'
+    receiver_address = 'e53d78c1c6fc694a0f29b3f24bee439338acbe3e'
+    receiver_did = f'did:ala:quor:redT:{receiver_address}'
+    receiver_presentation_hash = '0x1234'
+
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.return_value = 'getReceiverPresentationStatusReturnValue'
+
     expected_transaction = {
         'to': Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS),
-        'data': 'getIssuerCredentialStatusReturnValue',
+        'data': 'getReceiverPresentationStatusReturnValue',
         'gasPrice': 0,
         'gas': 600000,
         'nonce': '0x0'
     }
-    service = CredentialRegistryService(web3_mock)
 
-    transaction = service.get_issuer_credential_status(
-        issuer_did, issuer_credential_hash)
+    service = PresentationRegistryService(web3_mock)
 
-    mock_alastria_credential_registry.assert_called_with(web3_mock)
-    mock_alastria_credential_registry(web3_mock).encodeABI.assert_called_with(
-        fn_name='getIssuerCredentialStatus', args=[issuer_address, issuer_credential_hash])
+    transaction = service.get_receiver_presentation_status(
+        receiver_did, receiver_presentation_hash
+    )
+
+    mock_alastria_presentation_registry.assert_called_with(web3_mock)
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.assert_called_with(
+        fn_name='getReceiverPresentationStatus',
+        args=[receiver_address, receiver_presentation_hash]
+    )
+
     assert asdict(transaction) == expected_transaction
 
 
-@patch('alastria_identity.services.CredentialRegistryService.delegated')
-@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaCredentialRegistry')
-def test_update_credential_status(
-        mock_alastria_credential_registry,
-        mock_delegated):
-    web3_mock = Mock()
-    status = 2
-    issuer_credential_hash = '0x1234'
-    mock_alastria_credential_registry(
-        web3_mock).encodeABI.return_value = 'updateCredentialStatusReturnValue'
-    mock_delegated.return_value = 'delegatedReturnValue'
-    expected_transaction = {
-        'to': Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS),
-        'data': 'delegatedReturnValue',
-        'gasPrice': 0,
-        'gas': 600000,
-        'nonce': '0x0'
-    }
-    service = CredentialRegistryService(web3_mock)
+@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaPresentationRegistry')
+def test_get_presentation_status(
+    mock_alastria_presentation_registry
+):
 
-    transaction = service.update_credential_status(
-        issuer_credential_hash, status)
-
-    mock_alastria_credential_registry.assert_called_with(web3_mock)
-    mock_alastria_credential_registry(web3_mock).encodeABI.assert_called_with(
-        fn_name='updateCredentialStatus', args=[issuer_credential_hash, status])
-    mock_delegated.assert_called_with('updateCredentialStatusReturnValue')
-    assert asdict(transaction) == expected_transaction
-
-
-@patch('alastria_identity.services.identity_manager.ContractsService.AlastriaCredentialRegistry')
-def test_get_credential_status(
-        mock_alastria_credential_registry):
     web3_mock = Mock()
     subject_status = 2
-    issuer_status = 2
-    mock_alastria_credential_registry(
-        web3_mock).encodeABI.return_value = 'getCredentialStatusReturnValue'
+    receiver_status = 2
+
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.return_value = 'getPresentationStatusReturnValue'
+
     expected_transaction = {
         'to': Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS),
-        'data': 'getCredentialStatusReturnValue',
+        'data': 'getPresentationStatusReturnValue',
         'gasPrice': 0,
         'gas': 600000,
         'nonce': '0x0'
     }
-    service = CredentialRegistryService(web3_mock)
+    service = PresentationRegistryService(web3_mock)
 
-    transaction = service.get_credential_status(subject_status, issuer_status)
+    transaction = service.get_presentation_status(subject_status, receiver_status)
 
-    mock_alastria_credential_registry.assert_called_with(web3_mock)
-    mock_alastria_credential_registry(web3_mock).encodeABI.assert_called_with(
-        fn_name='getCredentialStatus', args=[subject_status, issuer_status])
+    mock_alastria_presentation_registry.assert_called_with(web3_mock)
+    mock_alastria_presentation_registry(
+        web3_mock
+    ).encodeABI.assert_called_with(
+        fn_name='getPresentationStatus',
+        args=[subject_status, receiver_status]
+    )
+
     assert asdict(transaction) == expected_transaction
 
 
 @patch('alastria_identity.services.identity_manager.ContractsService.AlastriaIdentityManager')
 def test_delegated_call(
-        mock_alastria_identity_manager):
+    mock_alastria_identity_manager
+):
+
     web3_mock = Mock()
     data = 'exampleData'
+
     mock_alastria_identity_manager(
-        web3_mock).encodeABI.return_value = 'delegatedValue'
-    service = CredentialRegistryService(web3_mock)
+        web3_mock
+    ).encodeABI.return_value = 'delegatedValue'
+    service = PresentationRegistryService(web3_mock)
 
     transaction = service.delegated(data)
 
     mock_alastria_identity_manager.assert_called_with(web3_mock)
     mock_alastria_identity_manager(web3_mock).encodeABI.assert_called_with(
         fn_name='delegateCall',
-        args=[Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS), 0, data])
+        args=[Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS), 0, data]
+    )
+
     assert transaction == 'delegatedValue'
