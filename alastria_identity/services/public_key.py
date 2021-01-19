7c89d002bcdb96e91b03b297be42cafad8ab2404
Victoria Pérez <60664274+victoriatnp@users.noreply.github.com>
2021-01-19 20:49:55 +0100

    Service to public key (#11)

---
+++ alastria_identity/services/public_key.py
@@ -0,0 +1,61 @@
+from web3 import Web3
+
+from alastria_identity.types import (Transaction, NetworkDid)
+from alastria_identity.services import ContractsService, IDENTITY_MANAGER_ADDRESS
+
+
+class PublicKeyService:
+    def __init__(self, endpoint: Web3):
+        self.endpoint = endpoint
+
+    def add_key(self, public_key: str) -> Transaction:
+        return self._build_transaction(
+                "addKey",
+                [public_key],
+                delegated=True)
+
+    def revoke_public_key(self, public_key: str) -> Transaction:
+        return self._build_transaction(
+                "revokePublicKey",
+                [public_key],
+                delegated=True)
+
+    def delete_public_key(self, public_key: str) -> Transaction:
+        return self._build_transaction(
+                "deletePublicKey",
+                [public_key],
+                delegated=True)
+
+
+    def get_current_public_key(self, did: str) -> Transaction:
+        subject_address = NetworkDid.from_did(did).proxy_address
+        return self._build_transaction(
+                "getCurrentPublicKey",
+                [subject_address],
+                delegated=False)
+
+    def get_public_key_status(self, did: str, public_key: str) -> Transaction:
+        subject_address = NetworkDid.from_did(did).proxy_address
+        return self._build_transaction(
+                "getPublicKeyStatus",
+                [subject_address, public_key],
+                delegated=False)
+
+    def _build_transaction(self, function_name: str, args: list, delegated: bool) -> Transaction:
+        encoded_abi = ContractsService.AlastriaPublicKeyRegistry(self.endpoint).encodeABI(
+            fn_name=function_name,
+            args=args
+        )
+
+        data = self.delegated(encoded_abi) if delegated else encoded_abi
+
+        return Transaction(
+            to=Web3.toChecksumAddress(IDENTITY_MANAGER_ADDRESS),
+            data=data)
+
+    def delegated(self, delegated_data) -> str:
+        return ContractsService.AlastriaIdentityManager(self.endpoint).encodeABI(
+            fn_name='delegateCall',
+            args=[Web3.toChecksumAddress(
+                IDENTITY_MANAGER_ADDRESS), 0, delegated_data]
+        )
